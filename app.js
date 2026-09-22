6c301c41dc8440ac7d1b6e14c2bea710795d76aa
moiseschelne-cyber <moises_chelne@estudante.sesisenai.org.br>
2026-09-21 21:05:02 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("____________________");
+console.log("___MEU_APLICATIVO___");
+console.log("____________________");
+
+console.log("Nome: Moisés de Lima Chelne");
+console.log("Cidade: Schroeder-SC");
+console.log("Atividade de interesse: Futsal");
+console.log("Quero aprender: Jogar futsal sem medo");
+console.log("Area de interesse: O Desenvolvimento de Sistemas");
+console.log("Dá um dez ai professor😁😁😁😁");
